414cc8b3924e1c48ef5a34cd61a7f83eb69e163d
Bogdan <null>
2021-12-24 20:28:07 +0200

    short aliases

--- DB/SelectDebtors.sql
+++ DB/SelectDebtors.sql
@@ -1,5 +1,5 @@
-select vali_schema.student.student_id, vali_schema.student.nume, vali_schema.student.grupa, vali_schema.examen.materie, vali_schema.examen.data
-from vali_schema.student
-inner join vali_schema.examen on vali_schema.student.grupa = vali_schema.examen.grupa
-inner join vali_schema.nota_examen on vali_schema.examen.examen_id = vali_schema.nota_examen.examen_id
-where vali_schema.nota_examen.nota < 5;
+select s.student_id, s.nume, s.grupa, e.materie, e.data
+from vali_schema.student s
+inner join vali_schema.examen e on s.grupa = e.grupa
+inner join vali_schema.nota_examen n on e.examen_id = n.examen_id
+where n.nota < 5;
