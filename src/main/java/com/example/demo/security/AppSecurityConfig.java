7d764e30504017506058b6537d258bc5a7f05f67
Valentin Popescu <MariusValentin.Popescu@endava.com>
2022-04-13 17:36:22 +0300

    Added more stuff. ~1.57 hour mark

--- src/main/java/com/example/demo/security/AppSecurityConfig.java
+++ src/main/java/com/example/demo/security/AppSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.demo.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,13 +13,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-import static com.example.demo.security.AppUserPermission.READ;
-import static com.example.demo.security.AppUserPermission.WRITE;
 import static com.example.demo.security.AppUserRole.*;
-import static org.springframework.http.HttpMethod.*;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     private final PasswordEncoder passwordEncoder;
 
@@ -32,11 +31,11 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable()
             .authorizeRequests()
             .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
-            .antMatchers(POST, "/api/v*/**").hasAuthority(WRITE.name())
-            .antMatchers(PUT, "/api/v*/**").hasAuthority(WRITE.name())
-            .antMatchers(DELETE, "/api/v*/**").hasAuthority(WRITE.name())
+//            .antMatchers(POST, "/api/v*/**").hasAuthority(WRITE.name())
+//            .antMatchers(PUT, "/api/v*/**").hasAuthority(WRITE.name())
+//            .antMatchers(DELETE, "/api/v*/**").hasAuthority(WRITE.name())
 //            .antMatchers(GET,"/api/v*/**").hasAnyRole(ADMIN.name(), SUPER_ADMIN.name())
-            .antMatchers(GET,"/api/v*/**").hasAuthority(READ.name())
+//            .antMatchers(GET,"/api/v*/**").hasAuthority(READ.name())
             .anyRequest()
             .authenticated().and().httpBasic();
     }
--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,6 +14,7 @@ public class StudentController {
             new Student(3, "Leana"));
 
     @GetMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('READ')")
     public Student getStudent(@PathVariable("studentId") Integer studentId) {
         return STUDENTS.stream()
                        .filter(student -> studentId.equals(student.getStudentId()))
@@ -21,21 +23,25 @@ public class StudentController {
     }
 
     @GetMapping
+    @PreAuthorize("hasAuthority('READ')")
     public List<Student> getAllStudents() {
         return STUDENTS;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('WRITE')")
     public void addStudent(@RequestBody Student student) {
         System.out.println(student);
     }
 
     @DeleteMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('WRITE')")
     public void removeStudent(@PathVariable("studentId") Integer studentId) {
         System.out.println(studentId);
     }
 
     @PutMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('WRITE')")
     public void updateStudent(@PathVariable("studentId") Integer studentId,
                               @RequestBody Student student) {
         System.out.printf("%d %s%n", studentId, student);
